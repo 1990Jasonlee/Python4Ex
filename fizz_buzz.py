90c08eadfdb90d3efb4cffe9a054245baa684808
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-14 16:15:57 -0400

    fizzbuzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,5 +1,7 @@
 for fizzbuzz in range(100):
-    if fizzbuzz % 3 == 0:
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print('FizzBuzz')
+    elif fizzbuzz % 3 == 0:
         print('Fizz')
     elif fizzbuzz % 5 == 0:
         print('Buzz')
