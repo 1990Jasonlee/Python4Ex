8cec5c64da3c78cec4ff823146870c33f47fb60f
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-14 17:07:01 -0400

    done

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial():
+    number = int(input('Enter number between 0 - 994:'))
+    num = 1
+    if number >= 1:
+        for i in range(1, number + 1):
+            num = num * i
+        print(num)
+    else:
+        print("0")
+
+
+factorial()
